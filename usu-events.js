ea2e3d9186e497d27048f42533c97779b9617ce6
Alex Lutz <alexinslc@gmail.com>
2015-03-28 15:52:18 -0600

    Reversed my idea. Oh, well.

--- usu-events.js
+++ usu-events.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var resource = require('express-resource');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var events = require('./routes/events');
