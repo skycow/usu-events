846f62486bd3ce125542f98da0ff2ec0761aae4e
anhany <an.han@aggiemail.usu.edu>
2015-04-25 22:48:21 -0600

    Turns out there is a central calendar, which happens to be a RSS feed. Lucky us.

--- utils/scrape.js
+++ utils/scrape.js
@@ -10,17 +10,88 @@ urls = [
                 +'calendar/displayBy=next10/rss=true'
     },
     {
+        title: 'eng',
+        url: 'http://engineering.usu.edu/htm/calendar/rss=true'
+    },
+    {
         title: 'cca',
         url: 'http://cca.usu.edu/events/'
     }
 ];
 
-hsbUrl = 'http://huntsman.usu.edu/fjmcenter/htm/'
-            +'calendar/displayBy=next10/rss=true';
+eventUrl = 'http://www.usu.edu/calendar/rss/eventcalendar.xml';
+ususaUrl = 'http://www.usu.edu/ususa/calendar/';
 
 var $, calendarItem;
 
-// request(hsbUrl, function(error,response,html) {
+// turns out, USU has a main event calendar rss... doh!
+// request(eventUrl, function(error,response,html) {
+//     if(!error) {
+//         $ = cheerio.load(html, {
+//                 xmlMode: true,
+//                 normalizeWhiteSpace: true
+//         });
+//         var mainCalendar = [];
+
+//         $('item').each(function(idx,el) {
+//             calendarItem = {};
+
+//             calendarItem.title = $(el).children('title').text();
+//             calendarItem.link = $(el).children('guid').text();
+//             calendarItem.category = $(el).children('category').text();
+//             calendarItem.description = $(el).children('description').text();
+//             calendarItem.location = $(el).children('location').text();
+//             calendarItem.date = $(el).children('pubDate').text();
+
+//             mainCalendar.push(calendarItem);
+
+//         });
+//         console.log(mainCalendar);
+//     }
+// });
+
+//  ...and USUSA has a calendar of their own
+request(ususaUrl, function(error,reponse,html) {
+    if(!error) {
+        $ = cheerio.load(html, {
+                normalizeWhiteSpace: true
+        });
+
+        var ususaCalendar = [], temp;
+
+        $('#content-container .row').each(function(idx,el) {
+            calendarItem = {};
+
+            calendarItem.title = $(el).children('h3').text();
+            calendarItem.date = $(el).children('.date').text();
+            calendarItem.description = $(el).children('p').text();
+
+            temp = $(el).find('.descrow')[0];
+            calendarItem.time = $(temp).text();
+
+            temp = $(el).find('a')[1];
+            calendarItem.link = 'http://www.usu.edu' + 
+                $(temp).attr('href');
+
+            temp = $(el).find('.desc2')[0];
+            calendarItem.location = $(temp).text();
+
+            // testvar = $(el).find('a')[1];
+
+            // console.log( $(testvar).attr('href') );
+
+            // calendarItem.time = $(el).find('.descrow')[0].text();
+            // calendarItem.link = $(el).children('.descrow')[1].children('.desc2')[1].html();
+
+            console.log(calendarItem);
+
+        });
+    }
+});
+
+
+// go get the business calendar rss feed, and scrape it
+// request(urls[0].url, function(error,response,html) {
 //     if(!error) {
 //         $ = cheerio.load(html, { 
 //                 xmlMode: true,
@@ -43,41 +114,66 @@ var $, calendarItem;
 //     }
 // });
 
-request(urls[1].url, function(error,response,html) {
-    if(!error) {
-        $ = cheerio.load(html, {
-            normalizeWhitespace: true,
-            decodeEntities: true
-        });
+// go get the engineering calendar rss feed, and scrape it
+// request(urls[1].url, function(error,response,html) {
+//     if(!error) {
+//         $ = cheerio.load(html, { 
+//                 xmlMode: true,
+//                 normalizeWhitespace: true
+//             });
+//         // var calendarItem;
+//         var engCalendar = [];
 
-        var ccaCalendar = [];
-        var testVar;
+//         $('item').each(function(idx,el) {
+//             calendarItem = {};
 
-        $('.rssFeed').each(function(idx,el) {
-            calendarItem = {};
-            calendarItem.link = $(el).children('h3').children('a').attr('href');
-            calendarItem.title = $(el).children('h3').children('a').html();
+//             calendarItem.title = $(el).children('title').text();
+//             calendarItem.link = $(el).children('link').text();
+//             calendarItem.description = $(el).children('description').text();
+//             calendarItem.date = $(el).children('pubDate').text();
 
-            $(el).children('p').each(function(idx,nestEl) {
-                content = $(nestEl).html();
+//             engCalendar.push(calendarItem);
+//         });
+//         // console.log(engCalendar);
+//     }
+// });
 
-                // if(content)
+// go get the cca calendar rss feed, and scrape it
+// request(urls[2].url, function(error,response,html) {
+//     if(!error) {
+//         $ = cheerio.load(html, {
+//             normalizeWhitespace: true,
+//             decodeEntities: true
+//         });
 
-                console.log(content);
-            });
+//         var ccaCalendar = [];
+//         var testVar;
 
-            // console.log(calendarItem);
+//         $('.rssFeed').each(function(idx,el) {
+//             calendarItem = {};
+//             calendarItem.link = $(el).children('h3').children('a').attr('href');
+//             calendarItem.title = $(el).children('h3').children('a').html();
 
-            // testVar = $(el).children('p').contents();
+//             $(el).children('p').each(function(idx,nestEl) {
+//                 content = $(nestEl).html();
 
-            // console.log(testVar);
-            // console.log($(el).children('p'));
+//                 // if(content)
 
-            // calendarItem.description = $(el).children('p')[1].text();
-            // calendarItem.date = $(el).children('p')[0].html();
+//                 console.log(content);
+//             });
 
-            ccaCalendar.push(calendarItem);
-        });
-        // console.log(ccaCalendar);
-    }
-});
+//             // console.log(calendarItem);
+
+//             // testVar = $(el).children('p').contents();
+
+//             // console.log(testVar);
+//             // console.log($(el).children('p'));
+
+//             // calendarItem.description = $(el).children('p')[1].text();
+//             // calendarItem.date = $(el).children('p')[0].html();
+
+//             ccaCalendar.push(calendarItem);
+//         });
+//         // console.log(ccaCalendar);
+//     }
+// });
