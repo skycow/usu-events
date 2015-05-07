50b0568d8c62d2388aa14dd1dcd2df115be86bb7
anhany <an.han@aggiemail.usu.edu>
2015-05-06 21:49:43 -0600

    Moved hsb and eng feeds into one file. Happy scraping!

--- utils/scrape.js
+++ utils/scrape.js
@@ -30,17 +30,47 @@ function hsbInit(callback) {
 	             item.date = $(el).children('pubDate').text();
 	             calendar.push(item);
 	         });
-			 console.log("inside request:", calendar);
 			 callback(calendar);
 	    }
 	});
 };
+function engInit(callback) {
+	var url = 'http://engineering.usu.edu/htm/calendar/rss=true';
+        var calendar = [];
+        var $, item;
+        request(url, function(error, resp, html) {
+        	if(!error) {
+                 $ = cheerio.load(html, { 
+                         xmlMode: true,
+                         normalizeWhitespace: true
+                     });
+        
+                 $('item').each(function(idx,el) {
+                     item = {};
+                     item.title = $(el).children('title').text();
+                     item.link = $(el).children('link').text();
+                     item.description = $(el).children('description').text();
+                     item.date = $(el).children('pubDate').text();
+                     calendar.push(item);
+                 });
+				 callback(calendar);
+            }
+        });
+};
+//function ccaInit(callback) {
+//	callback();
+//};
 
 var hsbFeed = hsbInit(function(resp) {
-	console.log("\nIniside callback of hsbInit\n\n",resp);
+	console.log("\n\nInside callback of hsbInit\n",resp);
 });
+var engFeed = engInit(function(resp) {
+	console.log("\n\nInside callback of engInit\n",resp);
+});
+//var ccaFeed = ccaInit(function(resp) {
+//	console.log("\n\nInside callback of ccaInit\n",resp);
+//});
 
-console.log("\nhsb.feed()\n",hsbFeed);
 
 
 
